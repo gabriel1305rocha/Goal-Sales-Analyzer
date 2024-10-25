89529550b08f7d5c4b202fd2dfa46a41769ccb0c
Gabriel Rocha de Melo <gabriel1305rocha@gmail.com>
2024-10-25 16:12:38 -0300

    Update Http FrameWork to Beego

--- srv/controllers/hello.world.go
+++ srv/controllers/hello.world.go
@@ -1,35 +1,28 @@
-package controller
+package controllers
 
 import (
-	"net/http"
-
-	"gorm.io/gorm"
+	"github.com/astaxie/beego"
 )
 
-func HelloWorld(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		//Set the response content type to HTML
-		w.Header().Set("Content-Type", "text/html")
+type HelloController struct {
+	beego.Controller
+}
 
-		html := `
-            <!DOCTYPE html>
-            <html lang="pt-BR">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Hello World</title>
-            </head>
-            <body>
-                <h1>Hello World</h1>
-                <p>Welcome to Go Application</p>
-            </body>
-            </html>
-        `
-		w.Write([]byte(html))
-		return
-	}
-
-	//For other methods, you can return a method not allowed.
-	w.WriteHeader(http.StatusMethodNotAllowed)
-	w.Write([]byte("Method Not Allowed."))
+func (c *HelloController) Get() {
+	c.Ctx.Output.ContentType("text/html")
+	c.Data["content"] = `
+		<!DOCTYPE html>
+		<html lang="pt-BR">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Hello World</title>
+		</head>
+		<body>
+			<h1>Hello World</h1>
+			<p>Welcome to Goal Sales Analyzer with Beego</p>
+		</body>
+		</html>
+	`
+	c.Ctx.WriteString(c.Data["content"].(string))
 }
